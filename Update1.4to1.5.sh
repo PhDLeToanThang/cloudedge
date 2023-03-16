19d38b8425c75d9f502d01c73f336240c0065c75
PhD Le Toan Thang <106635733+PhDLeToanThang@users.noreply.github.com>
2023-03-16 20:57:39 +0700

    Update Update1.4to1.5.sh

--- Update1.4to1.5.sh
+++ Update1.4to1.5.sh
@@ -2,21 +2,46 @@
 # Scenarios: Can you describe in detail the steps to upgrade from Guacamole 1.4.0 to Guacamole 1.5.0 ?
 # without losing data or without interrupting the user's web service?
 #
-# 1. Backup your current Guacamole installation and database.
-# 2. Download the latest version of Guacamole from the official website.
-# 3. Stop the Tomcat service using the following command:
+# Step 1. Backup your current Guacamole installation and database.
+# Step 2. Download the latest version of Guacamole from the official website.
+# Step 3. Stop the Tomcat service using the following command:
 
 sudo systemctl stop tomcat
-#4. Extract the downloaded Guacamole archive and copy guacamole.war to Tomcat's webapps directory:
+# Step 4. Extract the downloaded Guacamole archive and copy guacamole.war to Tomcat's webapps directory:
 cd /opt
 wget https://dlcdn.apache.org/guacamole/1.5.0/source/guacamole-server-1.5.0.tar.gz
+wget https://dlcdn.apache.org/guacamole/1.5.0/source/guacamole-client-1.5.0.tar.gz
+wget https://dlcdn.apache.org/guacamole/1.5.0/binary/guacamole-auth-duo-1.5.0.tar.gz
+wget https://dlcdn.apache.org/guacamole/1.5.0/binary/guacamole-auth-header-1.5.0.tar.gz
+wget https://dlcdn.apache.org/guacamole/1.5.0/binary/guacamole-auth-jdbc-1.5.0.tar.gz
+wget https://dlcdn.apache.org/guacamole/1.5.0/binary/guacamole-auth-json-1.5.0.tar.gz
+wget https://dlcdn.apache.org/guacamole/1.5.0/binary/guacamole-auth-ldap-1.5.0.tar.gz
+wget https://dlcdn.apache.org/guacamole/1.5.0/binary/guacamole-auth-quickconnect-1.5.0.tar.gz
+wget https://dlcdn.apache.org/guacamole/1.5.0/binary/guacamole-auth-sso-1.5.0.tar.gz
+wget https://dlcdn.apache.org/guacamole/1.5.0/binary/guacamole-auth-totp-1.5.0.tar.gz
+wget https://dlcdn.apache.org/guacamole/1.5.0/binary/guacamole-history-recording-storage-1.5.0.tar.gz
+wget https://dlcdn.apache.org/guacamole/1.5.0/binary/guacamole-vault-1.5.0.tar.gz
 tar -xzf guacamole-1.5.0.war.gz
+sudo cp /var/lib/tomcat9/webapps/guacamole.war /var/lib/tomcat9/webapps/guacamole145war
+sudo rm /var/lib/tomcat9/webapps/guacamole.war
 sudo cp guacamole.war /var/lib/tomcat9/webapps/
 
-#5. Remove old extensions by deleting all files in `/etc/guacamole/extensions` directory:
+# Step 5. Remove old extensions by deleting all files in `/etc/guacamole/extensions` directory:
+tar -xzf guacamole-client-1.5.0.tar.gz
+tar -xzf guacamole-auth-duo-1.5.0.tar.gz
+tar -xzf guacamole-auth-header-1.5.0.tar.gz
+tar -xzf guacamole-auth-jdbc-1.5.0.tar.gz
+tar -xzf guacamole-auth-json-1.5.0.tar.gz
+tar -xzf guacamole-auth-ldap-1.5.0.tar.gz
+tar -xzf guacamole-auth-quickconnect-1.5.0.tar.gz
+tar -xzf guacamole-auth-sso-1.5.0.tar.gz
+tar -xzf guacamole-auth-totp-1.5.0.tar.gz
+tar -xzf guacamole-history-recording-storage-1.5.0.tar.gz
+tar -xzf uacamole-vault-1.5.0.tar.gz
+
 sudo rm /etc/guacamole/extensions/*
 
-#6. Copy all `.jar` files from `guacamole-1.5.0.war` archive to `/etc/guacamole/extensions/` directory:
+# Step 6. Copy all `.jar` files from `guacamole-1.5.0.war` archive to `/etc/guacamole/extensions/` directory:
 sudo mkdir /etc/guacamole/extensions/
 sudo cp guacamole-auth-jdbc-mysql-1.5.0.jar /etc/guacamole/extensions/
 sudo cp mysql-connector-java-8.x.x.jar /etc/guacamole/extensions/
